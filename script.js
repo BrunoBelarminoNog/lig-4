821bab4060759c106bc6b7b2aec298f3a9e350f7
Guilherme Lemes de Freitas <guilemes300@gmail.com>
2021-04-05 14:05:08 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 /* variaveis e constantes */
-
+let game_table = [[0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0]]
+let first_player_turn = true
 /* variaveis e constantes */
 
 
@@ -24,5 +30,47 @@
 
 
 /* function reinicar game */
-
+function init_game(){
+    game_table = [[0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0]]
+    create_table()
+}
 /* function reinicar game */
+
+
+/* function criar tabela */
+function create_table(){
+    game_screen.innerHTML = ``
+    for(let row in game_table){
+        row_div = document.createElement("div")
+        row_div.classList.add("row")
+        for(let column in game_table[row]){
+            let div = document.createElement("div");
+            div.dataset.column_value = column.toString();
+            div.dataset.row_value = row.toString();
+            if(game_table[row][column] == 0){
+                div.classList.add("empty")
+            }
+            if(game_table[row][column] == "blue"){
+                div.classList.add("blue")
+            }
+            if(game_table[row][column] == "red"){
+                div.classList.add("red")
+            }
+            row_div.appendChild(div)
+        }
+        game_screen.appendChild(row_div)
+    }
+    let slots = document.querySelectorAll(".empty")
+    slots.forEach(element => {
+        element.addEventListener("click", e =>{
+            let column_selected = element.dataset.column_value
+            put_piece(column_selected)
+        })
+    });
+}
+/* function criar tabela */
