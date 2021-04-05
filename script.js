7d8ee11f39b5925b44659b829b946d44fecb02df
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-04-05 13:15:36 -0300

    Fix: Comentarios dividindo  Arquivo JS

---
+++ script.js
@@ -0,0 +1,28 @@
+/* variaveis e constantes */
+
+/* variaveis e constantes */
+
+
+/* eventos de click  */
+
+/* eventos de click  */
+
+
+/* function revezamento de turno */
+
+/* function revezamento de turno */
+
+
+/* function verificar ou bloquear movimento em coluna cheia */
+
+/* function verificar ou bloquear movimento em coluna cheia */
+
+
+/* function verificar resultado da partida */ 
+
+/* function verificar resultado da partida */ 
+
+
+/* function reinicar game */
+
+/* function reinicar game */
