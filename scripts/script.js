baf95a6c3947b6aabb5427a3d2fe1ec94e658c8a
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-04-08 12:00:38 -0300

    Fix: viewport mobile ajust

--- scripts/script.js
+++ scripts/script.js
@@ -380,3 +380,8 @@ btnCloseCredits.addEventListener('click', ()=> {
   containerCredits.classList.add('hidden');
   containerHomePage.classList.remove('hidden');
 })
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
